a2716f7a0bd3beedb2fba913d24331027a0bc326
Anubhav Ghosh <42681239+anubhavghosh@users.noreply.github.com>
2019-03-06 14:21:29 +0530

    Update index.php

--- index.php
+++ index.php
@@ -104,17 +104,7 @@
       $("#response").text(conversation.join(""));
     }
     var conversation = [];
-  </script>
-  <script type="text/javascript">
-    /* NOTE : Use web server to view HTML files as real-time update will not work if you directly open the HTML file in the browser. */
-    (function(d, m){
-      var kommunicateSettings = {"appId":"16c3efad7b70ff3519d6431f6865e7e50","conversationTitle":"GILabs "};
-      var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
-      s.src = "https://api.kommunicate.io/v2/kommunicate.app";
-      var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
-      window.kommunicate = m; m._globals = kommunicateSettings;
-    })(document, window.kommunicate || {});
-</script>
+    </script>
 
   
     <style>
@@ -125,8 +115,7 @@
       height: 600px;
       padding: 10px;
       background-color: #EAE2E2;
-    }
-    
+    }   
   </style>
  <script type="text/javascript">
     /* NOTE : Use web server to view HTML files as real-time update will not work if you directly open the HTML file in the browser. */
