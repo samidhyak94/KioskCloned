331fff207cceb768a555d0f3acc8fd6b61641ebf
Anubhav Ghosh <42681239+anubhavghosh@users.noreply.github.com>
2019-03-08 12:49:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -18,18 +18,23 @@
     var baseUrl = "https://api.dialogflow.com/v1/";
     $(document).ready(function() {
       $("#input").keypress(function(event) {
-        if (event.which == 13) {
+        if (event.which == 13)
+        {
+          console.log("++event.which==13 WORKING");
           event.preventDefault();
           send();
           this.value = '';
         }
       });
-      $("#rec").click(function(event) {
+      $("#rec").click(function(event) 
+      {
         switchRecognition();
+        console.log("++SwitchRecognition() fired");  
       });
     });
     var recognition;
     function startRecognition() {
+      console.log("++inside startRecognition()");    
       recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition ||
       window.msSpeechRecognition)();
       recognition.onstart = function(event) {
@@ -53,6 +58,7 @@
     function stopRecognition() {
       if (recognition) {
         recognition.stop();
+        console.log("++RecognitionStopped");  
         recognition = null;
       }
       updateRec();
@@ -72,28 +78,37 @@
       $("#rec").text(recognition ? "Stop" : "Speak");
     }
     function send() {
-    //  var conversation = [];
+     console.log("++insideSend()_Function"); 
            
             var text = $("#input").val();
+            console.log("++ValueofText==>"+text); 
             conversation.push("Me: " + text + '\r\n');
       $.ajax({
+        console.log("++insideAjaxCode");   
         type: "POST",
+        console.log("++lineBefore_baseUrl");   
         url: baseUrl + "query?v=20150910",
+        console.log("++lineAfter_baseUrl");  
         contentType: "application/json; charset=utf-8",
         dataType: "json",
+        console.log("++lineBefore_headers");  
         headers: {
+          console.log("++lineBefore_Authorization"); 
           "Authorization": "Bearer " + accessToken
+          console.log("++Authorization_Done"); 
         },
         data: JSON.stringify({ query: text, lang: "en", sessionId: "somerandomthing" }),
         success: function(data) 
                 {
-          var respText = data.result.fulfillment.speech;
+                  console.log("++Success");   
+                  var respText = data.result.fulfillment.speech;
                   console.log("Respuesta: " + respText);
                   setResponse(respText);
-          responsiveVoice.speak(respText,"Hindi Female");
+                  responsiveVoice.speak(respText,"Hindi Female");
                   $("#response").scrollTop($("#response").height());
         },
         error: function() {
+          console.log("++Error");  
           setResponse("Internal Server Error");
         }
       });
