df6296aed326479668b54085be43ae98e8437730
samidhyak94 <46779805+samidhyak94@users.noreply.github.com>
2019-03-26 15:51:05 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="G.I Labs">
     <meta name="author" content="GIL">
- 
+
     <title>iVAC</title>
     <link rel="stylesheet" href="css/style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
@@ -15,29 +15,53 @@
    
     <script type="text/javascript" src="js/conversation.js"></script>
 
+    <style type="text/css">
+      @media screen and (max-width:800px) {
+        .open-button {
+          width:100%;
+        }
+      }
+    </style>
 
 </head>
 <body>
 <section>
-  <div>
-    <img src="css/images/GILabs_logo.png" alt="G.I Labs" class="topleft">
-  </div>
-    <h1 class="quote"></h1>
-    <button class="open-button" onclick="openForm()">Have a query?<b>Ask iVAC!</b></button>
-    <button class="open-button booking" onclick="openForm()"><b>Booking Bot!</b></button>
-    <button class="open-button certification" onclick="openForm()"><b>Certification Bot!</b></button>
-    <button class="open-button latest" onclick="openForm()"><b>Latest Events Bot!</b></button>
-    <button class="open-button upcoming" onclick="openForm()"><b>Upcoming Events Bot!</b></button>
-    <button class="open-button appointment" onclick="openForm()"><b>Appointment Bot!</b></button>
-  <div class="chat-popup" id="myForm">
-    
-    <div class="form-container">
-      <h1>AI CHATBOT</h1>
-      <div id="response" class="response"></div>
-      <input id="input" class="input" placeholder="Type message..." type="text">
-      <button id="rec"><b class="fa fa-microphone">Voice</b></button>
+  <div class="container">
+    <div class="top">
+      <div class="logo">
+        <img src="css/images/GILabs_logo.png" alt="G.I Labs" class="topleft">
+      </div>
+      <div class="quote">
+        <h1>Welcome to the world of AI</h1>
+      </div>
+    </div>
+    <div style="overflow: auto;">
+      <div class="left-menu">
+        <button class="open-button" onclick="openForm()">Something <b>New1</b></button>
+        <button class="open-button" onclick="openForm()">Something <b>New2</b></button>  
+        <button class="open-button" onclick="openForm()">Something <b>New3</b></button>
+        <button class="open-button" onclick="openForm()">Something <b>New4</b></button>
+        <button class="open-button" onclick="openForm()">Something <b>New5</b></button>
+      </div>
+      <div class="right-menu">
+        <button class="open-button fl-rt" onclick="openForm()">Have any query? <b>Ask AI Bot.</b></button>
+        <button class="open-button fl-rt" onclick="openForm()">Something <b>New6</b></button>
+        <button class="open-button fl-rt" onclick="openForm()">Something <b>New7</b></button>
+        <button class="open-button fl-rt" onclick="openForm()">Something <b>New8</b></button>
+        <button class="open-button fl-rt" onclick="openForm()">Something <b>New9</b></button>
+      </div>
+    </div> 
+  
+    <div class="chat-popup" id="myForm">
       
-      <button type="button" class="btn cancel" onclick="closeForm()"><b>CLOSE</b></button>
+      <div class="form-container">
+        <h1>AI CHATBOT</h1>
+        <div id="response" class="response"></div>
+        <input id="input" class="input" placeholder="Type message.." type="text">
+        <button id="rec"><b class="fa fa-microphone">Voice</b></button>
+        
+        <button type="button" class="btn cancel" onclick="closeForm()"><b>Close</b></button>
+      </div>
     </div>
   </div>
 </section>
